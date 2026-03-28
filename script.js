78eb4676d901a941d0c5f14db57ddee70d10c502
vishnuvardhan3195923-dot <vishnuvardhan3195923@gmail.com>
2026-03-28 19:40:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('.nav');
+
+  if (window.scrollY > 50) {
+    nav.style.background = 'rgba(0, 0, 0, 0.8)';
+  } else {
+    nav.style.background = 'transparent';
+  }
+});
