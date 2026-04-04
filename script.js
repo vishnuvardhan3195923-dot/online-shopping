df731124a1eee1889a90842893363f046278c930
vishnuvardhan3195923-dot <vishnuvardhan3195923@gmail.com>
2026-04-04 22:19:16 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-  const nav = document.querySelector('.nav');
-
-  if (window.scrollY > 50) {
-    nav.style.background = 'rgba(0, 0, 0, 0.8)';
-  } else {
-    nav.style.background = 'transparent';
-  }
-});
